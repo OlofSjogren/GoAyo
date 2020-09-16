260ca34839dbe65150fc8c47d3aa07b984589644
Oscar <o.sanner@hotmail.com>
2020-09-16 16:57:54 +0200

    reset TemplateClass.java to resolve merge issue

--- app/src/main/java/com/goayo/debtify/model/ModelEngine.java
+++ app/src/main/java/com/goayo/debtify/model/ModelEngine.java
@@ -204,5 +204,5 @@ public class ModelEngine {
     public boolean payOffDebt(double amount, String debtID){
         return account.payOffDebt(amount,debtID);
     }
-    
+
 }
--- app/src/main/java/com/goayo/debtify/model/TemplateClass.java
+++ app/src/main/java/com/goayo/debtify/model/TemplateClass.java
@@ -1,8 +1,6 @@
 package com.goayo.debtify.model;
 
 public class TemplateClass {
-
-
     /**
      * To make model package visible.
      */
