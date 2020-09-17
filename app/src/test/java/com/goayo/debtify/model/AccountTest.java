5f16ddbdaa55227b2a5887eb0ef5e6563dd95100
Puh00 <alex.phu@live.com>
2020-09-17 17:07:22 +0200

    Tests for Account class
    Co-authored-by: OlofSjogren <olofsjogren97@gmail.com>

---
+++ app/src/test/java/com/goayo/debtify/model/AccountTest.java
@@ -0,0 +1,54 @@
+package com.goayo.debtify.model;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountTest {
+
+    static Account account;
+    static MockDatabase database;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        database = new MockDatabase();
+        account = new Account(database);
+        account.loginUser("123", "asd");
+    }
+
+    @Test
+    public void registerUser() throws Exception {
+        account.registerUser("123", "Åke", "asd");
+        assertEquals("Åke", database.getUser("123").getName());
+    }
+
+    @Test
+    public void loginUser() throws Exception {
+        //TODO ("To be tested")
+    }
+
+    @Test
+    public void createGroup() {
+    }
+
+    @Test
+    public void addContact() {
+    }
+
+    @Test
+    public void addUserToGroup() {
+    }
+
+    @Test
+    public void removeUserFromGroup() {
+    }
+
+    @Test
+    public void createDebt() {
+    }
+
+    @Test
+    public void payOffDebt() {
+    }
+}
