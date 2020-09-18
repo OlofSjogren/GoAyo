484f3171c75c377db284684a5b0fd73a54a53c4c
yenanw <yenan.w829@gmail.com>
2020-09-18 14:57:51 +0200

    added views for add-debt and settle-debt
    Co-authored-by: gabrielbrattgard <gabriel.brattgard@gmail.com>

---
+++ app/src/main/java/com/goayo/debtify/view/AddDebtFragment.java
@@ -0,0 +1,76 @@
+package com.goayo.debtify.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import com.goayo.debtify.R;
+import com.goayo.debtify.databinding.AddDebtFragmentBinding;
+import com.goayo.debtify.model.ModelEngine;
+
+/**
+ * @author Alex Phu, Yenan Wang
+ * @date 2020-09-09
+ * <p>
+ * Page for adding a debt to a group.
+ *
+ * 2020-09-18 Modified by Yenan & Gabriel: Added AddDebt view, tested with hard-coded value
+ */
+public class AddDebtFragment extends Fragment {
+
+    private AddDebtFragmentBinding binding;
+    private ModelEngine model;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.add_debt_fragment, container, false);
+        model = new ModelEngine();
+
+        // TODO: remove hard-coded value
+        binding.lenderLinearLayout.addView(new Button(getContext()));
+        binding.lenderLinearLayout.addView(new Button(getContext()));
+        binding.lenderLinearLayout.addView(new Button(getContext()));
+        binding.lenderLinearLayout.addView(new Button(getContext()));
+        binding.lenderLinearLayout.addView(new Button(getContext()));
+        binding.lenderLinearLayout.addView(new Button(getContext()));
+        binding.lenderLinearLayout.addView(new Button(getContext()));
+        binding.lenderLinearLayout.addView(new Button(getContext()));
+        binding.lenderLinearLayout.addView(new Button(getContext()));
+        binding.lenderLinearLayout.addView(new Button(getContext()));
+        binding.lenderLinearLayout.addView(new Button(getContext()));
+        binding.lenderLinearLayout.addView(new Button(getContext()));
+        binding.lenderLinearLayout.addView(new Button(getContext()));
+        binding.lenderLinearLayout.addView(new Button(getContext()));
+        binding.borrowerLinearLayout.addView(new Button(getContext()));
+        binding.borrowerLinearLayout.addView(new Button(getContext()));
+        binding.borrowerLinearLayout.addView(new Button(getContext()));
+        binding.borrowerLinearLayout.addView(new Button(getContext()));
+        binding.borrowerLinearLayout.addView(new Button(getContext()));
+        binding.borrowerLinearLayout.addView(new Button(getContext()));
+        binding.borrowerLinearLayout.addView(new Button(getContext()));
+        binding.borrowerLinearLayout.addView(new Button(getContext()));
+        binding.borrowerLinearLayout.addView(new Button(getContext()));
+        binding.borrowerLinearLayout.addView(new Button(getContext()));
+        binding.borrowerLinearLayout.addView(new Button(getContext()));
+        binding.borrowerLinearLayout.addView(new Button(getContext()));
+
+        binding.createDebtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // model.createDebt(parameters)
+                Toast.makeText(getContext(), "clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return binding.getRoot();
+    }
+}
---
+++ app/src/main/java/com/goayo/debtify/view/SettleDebtFragment.java
@@ -0,0 +1,60 @@
+package com.goayo.debtify.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import com.goayo.debtify.R;
+import com.goayo.debtify.databinding.SettleDebtFragmentBinding;
+import com.goayo.debtify.model.ModelEngine;
+
+/**
+ * @author Alex Phu, Yenan Wang
+ * @date 2020-09-09
+ * <p>
+ * Page for paying off debt.
+ *
+ * 2020-09-18 Modified by Yenan & Gabriel: Added SettleDebt view, tested with hard-coded value
+ */
+public class SettleDebtFragment extends Fragment {
+
+    private SettleDebtFragmentBinding binding;
+    private ModelEngine model;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.settle_debt_fragment, container, false);
+        model = new ModelEngine();
+
+        // TODO: remove hard-coded value
+        binding.debtLinearLayout.addView(new Button(getContext()));
+        binding.debtLinearLayout.addView(new Button(getContext()));
+        binding.debtLinearLayout.addView(new Button(getContext()));
+        binding.debtLinearLayout.addView(new Button(getContext()));
+        binding.debtLinearLayout.addView(new Button(getContext()));
+        binding.debtLinearLayout.addView(new Button(getContext()));
+        binding.debtLinearLayout.addView(new Button(getContext()));
+        binding.debtLinearLayout.addView(new Button(getContext()));
+        binding.debtLinearLayout.addView(new Button(getContext()));
+        binding.debtLinearLayout.addView(new Button(getContext()));
+
+        binding.settleDebtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // model.settleDebt(parameters)
+                Toast.makeText(getContext(), "clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return binding.getRoot();
+    }
+}
