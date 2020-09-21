e040e61c0162ef6a5239b293d4e20f656c0908b6
OlofSjogren <olofsjogren97@gmail.com>
2020-09-21 16:13:09 +0200

    Added main controller aggregate in controller package

---
+++ app/src/main/java/com/goayo/debtify/controller/MainController.java
@@ -0,0 +1,10 @@
+package com.goayo.debtify.controller;
+
+/**
+ * @author Olof Sjögren
+ * @date 2020-09-21
+ * <p>
+ * Main controller aggregate for initializing different controllers.
+ */
+public class MainController {
+}
