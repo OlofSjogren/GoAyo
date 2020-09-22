3a7a3e8c4ef973ab2232feb1827067155a45b7c6
Puh00 <alex.phu@live.com>
2020-09-22 10:53:57 +0200

    Created xml files for detailed group view
    Co-authored-by: OscarSanner <o.sanner@hotmail.com>

---
+++ app/src/main/java/com/goayo/debtify/view/DetailedGroupActivity.java
@@ -0,0 +1,15 @@
+package com.goayo.debtify.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.goayo.debtify.R;
+
+
+public class DetailedGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_group);
+    }
+}
