c2620befc37610cf794e467144b83544c6c1da27
Puh00 <alex.phu@live.com>
2020-09-22 18:25:48 +0200

    Resolved issue with bottomNavigation

--- app/src/main/java/com/goayo/debtify/view/DetailedGroupActivity.java
+++ app/src/main/java/com/goayo/debtify/view/DetailedGroupActivity.java
@@ -1,21 +1,21 @@
 package com.goayo.debtify.view;
 
-import androidx.annotation.NonNull;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import com.goayo.debtify.R;
 import com.goayo.debtify.databinding.ActivityDetailedGroupBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-
+/**
+ * @author Alex Phu, Oscar Sanner
+ * @date 2020-09-22
+ * <p>
+ * Activity for the detailed view of a group.
+ */
 public class DetailedGroupActivity extends AppCompatActivity {
-
-    Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,29 +23,26 @@ public class DetailedGroupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detailed_group);
         ActivityDetailedGroupBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_detailed_group);
 
+        initToolBar(binding);
+        initBottomNavigation(binding);
     }
 
-    private void initToolBar(ActivityDetailedGroupBinding binding){
+    private void initToolBar(ActivityDetailedGroupBinding binding) {
         setSupportActionBar(binding.detailedGroupToolbar);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.detailed_group_bottom_navigation_menu, menu);
-        this.menu = menu;
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    private void initBottomNav(ActivityDetailedGroupBinding binding){
-        BottomNavigationView view = binding.detailedGroupBottomNavigation;
-        view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private void initBottomNavigation(ActivityDetailedGroupBinding binding) {
+        //Navigation
+        binding.detailedGroupAddDebtButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()){
-                    case menu.findItem(R.id.add)
-                        break;
-                }
-                return false;
+            public void onClick(View view) {
+                //TODO ("Navigate to the add-debt-view")
+            }
+        });
+        binding.detailedGroupAddPaymentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO ("Navigate to the settle-up-view")
             }
         });
     }
