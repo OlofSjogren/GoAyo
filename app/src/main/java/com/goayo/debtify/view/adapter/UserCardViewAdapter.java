5035fea46319866ee887e64be0f4cecf03c494a7
gabrielbrattgard <gabriel.brattgard@gmail.com>
2020-09-23 16:36:51 +0200

    Implemented UserCardViewAdapter.
    Co-authored-by: yenanw <yenan.w829@gmail.com>

--- app/src/main/java/com/goayo/debtify/view/adapter/UserCardViewAdapter.java
+++ app/src/main/java/com/goayo/debtify/view/adapter/UserCardViewAdapter.java
@@ -1,9 +1,18 @@
 package com.goayo.debtify.view.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.goayo.debtify.R;
+import com.goayo.debtify.modelaccess.IUserData;
+
+import org.w3c.dom.Text;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 
@@ -12,29 +21,85 @@ import androidx.recyclerview.widget.RecyclerView;
  * @date 2020-09-22
  * <p>
  * RecyclerView adapter for the user cardviews. Ensures that the correct information are shown on each cardItem and its respective listeners.
+ *
+ * 2020-09-23 Modified by Gabriel Brattgård, Yenan Wang: Implemented.
  */
 public class UserCardViewAdapter extends RecyclerView.Adapter<UserCardViewAdapter.UserCardViewHolder> {
 
-    @NonNull
-    @Override
-    public UserCardViewAdapter.UserCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    private IUserData[] dataSet;
+
+    /**
+     * Constructor for UserCardViewAdapter.
+     *
+     * @param dataSet Takes in a list of IUserData.
+     */
+    public UserCardViewAdapter(IUserData[] dataSet) {
+        this.dataSet = dataSet;
     }
 
+    /**
+     * Creates a new ViewHolder when it is needed.
+     *
+     * @param parent
+     * @param viewType
+     * @return A new instance of UserCardViewHolder
+     */
+    @NonNull
     @Override
-    public void onBindViewHolder(@NonNull UserCardViewAdapter.UserCardViewHolder holder, int position) {
+    public UserCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        CardView v = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.user_cardview, parent, false);
 
+        UserCardViewHolder vh = new UserCardViewHolder(v);
+        return vh;
+    }
+
+    /**
+     * Binds the data to the ViewHolder
+     *
+     * @param holder ViewHolder to bind data to.
+     * @param position Position in the dataSet array.
+     */
+    @Override
+    public void onBindViewHolder(@NonNull UserCardViewHolder holder, int position) {
+        holder.setUserData(dataSet[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dataSet.length;
     }
 
+
+    /**
+     * @author Yenan Wang, Gabriel Brattgård
+     * @date 2020-09-23
+     * <p>
+     * ViewHolder for UserCardView.
+     */
     static class UserCardViewHolder extends RecyclerView.ViewHolder {
 
-        public UserCardViewHolder(@NonNull View itemView) {
+        private TextView username;
+        private TextView phoneNumber;
+
+        /**
+         * Constructor for UserCardViewHolder
+         *
+         * @param itemView The UserCardView to bind data to.
+         */
+        UserCardViewHolder(@NonNull View itemView) {
             super(itemView);
+            username = itemView.findViewById(R.id.usernameTextView);
+            phoneNumber = itemView.findViewById(R.id.userPhoneNumberTextView);
         }
+
+        void setUserData(IUserData user) {
+            username.setText(user.getName());
+            phoneNumber.setText(user.getPhoneNumber());
+        }
+
+        void setCardViewListener(){
+         //Todo: Implement.
+        }
+
     }
 }
