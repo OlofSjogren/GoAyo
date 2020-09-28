fbe44c77b109509d417812ce48986a010bf79125
Gabriel <gabriel.brattgard@gmail.com>
2020-09-28 15:45:35 +0200

    Added DisplayContactsFragment and ContactsViewAdapter to display personal contacts list.

---
+++ app/src/main/java/com/goayo/debtify/view/DisplayContactsFragment.java
@@ -0,0 +1,61 @@
+package com.goayo.debtify.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.goayo.debtify.R;
+import com.goayo.debtify.databinding.DisplayContactsFragmentBinding;
+import com.goayo.debtify.modelaccess.IUserData;
+import com.goayo.debtify.view.adapter.ContactsViewAdapter;
+import com.goayo.debtify.view.adapter.UserCardViewAdapter;
+import com.goayo.debtify.viewModel.ContactsViewModel;
+import com.goayo.debtify.viewModel.ContactsViewModelFactory;
+
+import java.util.Set;
+
+/**
+ * @author Gabriel Brattgård
+ * @date 2020-09-28
+ *
+ * Fragment class to display a list of personal contacts to the logged in user.
+ * Has buttons to add or remove contacts to the list.
+ *
+ *
+ */
+public class DisplayContactsFragment extends Fragment {
+
+    DisplayContactsFragmentBinding binding;
+
+    public DisplayContactsFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.id.createGroupFragment, container, false);
+
+        ContactsViewModel viewModel = new ContactsViewModel();
+
+        initRecyclerView(viewModel.getContactsData().getValue());
+        return binding.getRoot();
+    }
+
+    private void initRecyclerView(Set<IUserData> userData){
+        RecyclerView contactsRecyclerView = binding.displayContactsRecyclerView;
+
+        ContactsViewAdapter adapter = new ContactsViewAdapter(this.getContext(), (IUserData[]) userData.toArray());
+        contactsRecyclerView.setAdapter(adapter);
+        contactsRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1));
+    }
+}
