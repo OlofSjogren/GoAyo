77238522c1bce1b5c0beab287e872f7c2e409b15
OscarSanner <o.sanner@hotmail.com>
2020-09-29 08:13:59 +0200

    Stared on tests for mock database

--- app/src/test/java/com/goayo/debtify/model/MockDatabaseTest.java
+++ app/src/test/java/com/goayo/debtify/model/MockDatabaseTest.java
@@ -3,6 +3,9 @@ package com.goayo.debtify.model;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,29 +16,51 @@ import static org.junit.Assert.*;
 
 public class MockDatabaseTest {
 
+    private IDatabase database;
+
     @Before
     public void setUp() throws Exception {
-        IDatabase database = new MockDatabase();
+        database = new MockDatabase();
     }
 
     @Test
     public void getGroups() {
+        Set<Group> groupSet = database.getGroups("0786458765");
+        assert (groupSet.contains(database.getGroupFromId("4116c93e-5542-4b5c-8423-010a901abdce")));
+        assert (groupSet.contains(database.getGroupFromId("1a705586-238d-4a29-b7af-36dc103bd45a")));
+        assert (groupSet.size() == 2);
     }
 
     @Test
     public void getGroupFromId() {
+        assert (database.getGroupFromId("1a705586-238d-4a29-b7af-36dc103bd45a").getGroupMembers().size() == 5);
+        assert (database.getGroupFromId("4116c93e-5542-4b5c-8423-010a901abdce").getGroupMembers().size() == 3);
+        assert (database.getGroupFromId("d467b5bc-5fa9-4ac2-890d-29a07803d484").getGroupMembers().size() == 3);
     }
 
     @Test
     public void getUser() {
+        assert (database.getUser("0738980732").getName().equals("Alex Phu"));
     }
 
     @Test
     public void registerUser() {
+        String testPhoneNumber = "0987123454"
+        database.registerUser(testPhoneNumber, "Ralf Broberg", "flar");
+        assert (database.getUser(testPhoneNumber).getName().equals("Ralf Broberg"));
     }
 
     @Test
     public void registerGroup() {
+        Set<User> userToBeAdded = new HashSet<>();
+        userToBeAdded.add(database.getUser("0701234546"));
+        userToBeAdded.add(database.getUser("0738980732"));
+        userToBeAdded.add(database.getUser("0733387676"));
+
+        String groupName = "Testgroup";
+
+        database.registerGroup()
+
     }
 
     @Test
