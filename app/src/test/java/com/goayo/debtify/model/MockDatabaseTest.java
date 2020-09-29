2a62af6f92e96d82a5708f5312e44daca3f6e63f
OscarSanner <o.sanner@hotmail.com>
2020-09-29 09:13:29 +0200

    Added more tests to the MockDatabase test file
    Co-authored-by: OlofSjogren <olofsjogren97@gmail.com>

--- app/src/test/java/com/goayo/debtify/model/MockDatabaseTest.java
+++ app/src/test/java/com/goayo/debtify/model/MockDatabaseTest.java
@@ -1,9 +1,13 @@
 package com.goayo.debtify.model;
 
+import com.goayo.debtify.modelaccess.IDebtData;
+import com.goayo.debtify.modelaccess.IPaymentData;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.*;
@@ -52,39 +56,133 @@ public class MockDatabaseTest {
 
     @Test
     public void registerGroup() {
-        Set<User> userToBeAdded = new HashSet<>();
-        userToBeAdded.add(database.getUser("0701234546"));
-        userToBeAdded.add(database.getUser("0738980732"));
-        userToBeAdded.add(database.getUser("0733387676"));
+        Set<String> userToBeAdded = new HashSet<>();
+        int oscarSizeBefore = database.getGroups("0701234546").size();
+        int alexSizeBefore = database.getGroups("0738980732").size();
+        int gabrielSizeBefore = database.getGroups("0733387676").size();
 
+
+        userToBeAdded.add("0701234546");
+        userToBeAdded.add("0738980732");
+        userToBeAdded.add("0733387676");
         String groupName = "Testgroup";
 
-        database.registerGroup()
+        database.registerGroup(groupName, userToBeAdded);
 
+        int oscarSizeAfter = database.getGroups("0701234546").size();
+        int alexSizeAfter = database.getGroups("0738980732").size();
+        int gabrielSizeAfter = database.getGroups("0733387676").size();
+
+        assert (gabrielSizeAfter - gabrielSizeBefore == 1);
+        assert (alexSizeAfter - alexSizeBefore == 1);
+        assert (oscarSizeAfter - oscarSizeBefore == 1);
     }
 
     @Test
-    public void addDebt() {
+    public void addDebt() throws Exception {
+
+        Set<String> alexBorrowerSet = new HashSet<>();
+        alexBorrowerSet.add("0738980732");
+
+        List<IDebtData> debtDataBefore = database.getGroupFromId("1a705586-238d-4a29-b7af-36dc103bd45a").getDebts();
+
+        try {
+            database.addDebt("1a705586-238d-4a29-b7af-36dc103bd45a", "0701234546", alexBorrowerSet, 20);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        List<IDebtData> debtDataAfter = database.getGroupFromId("1a705586-238d-4a29-b7af-36dc103bd45a").getDebts();
+        for(IDebtData debtData : debtDataBefore){
+            if(debtDataAfter.contains(debtData)){
+                debtDataAfter.remove(debtData);
+            }
+        }
+
+        assert (debtDataAfter.size() == 1);
+        assert (debtDataAfter.get(0).getAmountOwed() == 20);
+        assert (debtDataAfter.get(0).getBorrower().getName().equals("Alex Phu"));
+        assert (debtDataAfter.get(0).getLender().getName().equals("Oscar Sanner"));
+
+        assert (!(database.addDebt("Non existent Id", "Non existent number", new HashSet<String>(), 20)));
+        assert (!(database.addDebt("1a705586-238d-4a29-b7af-36dc103bd45a", "Non existent number", new HashSet<String>(), 20)));
+        assert (!(database.addDebt("1a705586-238d-4a29-b7af-36dc103bd45a", "0701234546", new HashSet<String>(), 20)));
+
     }
 
     @Test
     public void addContact() {
+        User alex = database.getUser("0738980732");
+        User oscar = database.getUser("0701234546");
+        assert (!(database.getContactList(alex.getPhoneNumber()).contains(oscar)));
+        database.addContact(alex.getPhoneNumber(), oscar.getPhoneNumber());
+        assert (database.getContactList(alex.getPhoneNumber()).contains(oscar));
     }
 
     @Test
     public void removeContact() {
+        User alex = database.getUser("0738980732");
+        User oscar = database.getUser("0701234546");
+
+        assert (database.getContactList(oscar.getPhoneNumber()).contains(alex));
+        database.removeContact(oscar.getPhoneNumber(), alex.getPhoneNumber());
+        assert (!(database.getContactList(oscar.getPhoneNumber()).contains(alex)));
     }
 
     @Test
     public void addPayment() {
+        Set<String> alexBorrowerSet = new HashSet<>();
+        alexBorrowerSet.add("0738980732");
+
+        List<IDebtData> debtDataBefore = database.getGroupFromId("1a705586-238d-4a29-b7af-36dc103bd45a").getDebts();
+
+        try {
+            database.addDebt("1a705586-238d-4a29-b7af-36dc103bd45a", "0701234546", alexBorrowerSet, 20);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        List<IDebtData> debtDataAfter = database.getGroupFromId("1a705586-238d-4a29-b7af-36dc103bd45a").getDebts();
+        for(IDebtData debtData : debtDataBefore){
+            if(debtDataAfter.contains(debtData)){
+                debtDataAfter.remove(debtData);
+            }
+        }
+
+        List<IPaymentData> paymentsBefore = debtDataAfter.get(0).getPaymentHistory();
+        try {
+            database.addPayment("1a705586-238d-4a29-b7af-36dc103bd45a", debtDataAfter.get(0).getDebtID(), 15);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        List<IPaymentData> paymentsAfter = debtDataAfter.get(0).getPaymentHistory();
+
+        assert (paymentsBefore.size() == 0);
+        assert (paymentsAfter.size() == 1);
+        assert (paymentsAfter.get(0).getPaidAmount() == 15);
+        assert (debtDataAfter.get(0).getAmountOwed() == 5);
+        assert (debtDataAfter.get(0).getOriginalDebt() - paymentsAfter.get(0).getPaidAmount() == debtDataAfter.get(0).getAmountOwed());
+
     }
 
     @Test
     public void addUserToGroup() {
+        User oscar = database.getUser("0701234546");
+        assert (!(database.getGroupFromId("d467b5bc-5fa9-4ac2-890d-29a07803d484").getGroupMembers().contains(oscar)));
+        database.addUserToGroup("d467b5bc-5fa9-4ac2-890d-29a07803d484", oscar.getPhoneNumber());
+        assert (database.getGroupFromId("d467b5bc-5fa9-4ac2-890d-29a07803d484").getGroupMembers().contains(oscar));
     }
 
     @Test
     public void getUserToBeLoggedIn() {
+        User oscar = database.getUserToBeLoggedIn("0701234546", "racso");
+        assert (oscar != null);
+        User noUser = database.getUserToBeLoggedIn("0987467281", "123");
+        assert (noUser == null);
+        User wrongPw = database.getUserToBeLoggedIn("0701234546", "123");
+        assert (wrongPw == null);
+        User wrongNumber = database.getUserToBeLoggedIn("0987467281", "racso");
+        assert (wrongNumber == null);
     }
 
     @Test
