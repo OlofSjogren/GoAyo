85e26a2c52d963abc646696f22fab25b2c4cbe39
yenanw <yenanw@yn-wg.com>
2020-10-12 11:50:24 +0200

    fix a problem where empty selection will still cause the view to update

--- app/src/main/java/com/goayo/debtify/view/AddDebtFragment.java
+++ app/src/main/java/com/goayo/debtify/view/AddDebtFragment.java
@@ -43,22 +43,19 @@ import java.util.Set;
  * return types and params of methods are correctly set as BigDecimal.
  */
 public class AddDebtFragment extends Fragment {
+    private final String LENDER_DATA = "LENDER_DATA";
+    private final String BORROWER_DATA = "BORROWER_DATA";
     // the binding object which preloads all its xml components
     private AddDebtFragmentBinding binding;
     // the ViewModel for this class
     private AddDebtViewModel addDebtViewModel;
     // the shared ViewModel for data retrieval
     private PickUserViewModel pickUserViewModel;
-
     // the two adapters which will observe the ViewModel
     private UserCardViewAdapter lenderAdapter;
     private UserCardViewAdapter borrowersAdapter;
-
     // the variable will decide what data it has retrieved
     private String dataRetrieved = "";
-
-    private final String LENDER_DATA = "LENDER_DATA";
-    private final String BORROWER_DATA = "BORROWER_DATA";
 
     @Nullable
     @Override
@@ -97,7 +94,6 @@ public class AddDebtFragment extends Fragment {
 
         // set a observer to notify the recyclerview adapter whenever the borrowers data has changed
         addDebtViewModel.getSelectedBorrowersData().observe(getViewLifecycleOwner(), new Observer<Set<IUserData>>() {
-
             @Override
             public void onChanged(Set<IUserData> userDataSet) {
                 borrowersAdapter.updateList(new ArrayList<>(userDataSet));
@@ -108,10 +104,13 @@ public class AddDebtFragment extends Fragment {
         pickUserViewModel.getSelectedUsersData().observe(getViewLifecycleOwner(), new Observer<List<IUserData>>() {
             @Override
             public void onChanged(List<IUserData> iUserData) {
-                if (dataRetrieved.equals(LENDER_DATA)) {
-                    addDebtViewModel.setSelectedLenderData(new HashSet<>(iUserData));
-                } else if (dataRetrieved.equals(BORROWER_DATA)) {
-                    addDebtViewModel.setSelectedBorrowersData(new HashSet<>(iUserData));
+                // ignore it if the list is empty
+                if (iUserData.size() != 0) {
+                    if (dataRetrieved.equals(LENDER_DATA)) {
+                        addDebtViewModel.setSelectedLenderData(new HashSet<>(iUserData));
+                    } else if (dataRetrieved.equals(BORROWER_DATA)) {
+                        addDebtViewModel.setSelectedBorrowersData(new HashSet<>(iUserData));
+                    }
                 }
             }
         });
