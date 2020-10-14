debd33a9991132030e00fef870abe3a4e1dea526
Oscar <o.sanner@hotmail.com>
2020-10-14 19:18:29 +0200

    Create documentation for the EntityFactory

--- app/src/main/java/com/goayo/debtify/model/EntityFactory.java
+++ app/src/main/java/com/goayo/debtify/model/EntityFactory.java
@@ -3,11 +3,33 @@ package com.goayo.debtify.model;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * @Author Oscar Sanner and Olof Sjögren.
+ * @date 2020-10-09
+ *
+ * A class with static methods for instantiating new objects.
+ */
+
 public class EntityFactory {
+
+    /**
+     * Creates a new user based on parameters.
+     *
+     * @param phoneNumber The number of the new user to be created.
+     * @param name The name of the new user to be created.
+     * @return A new user with specified properties.
+     */
     public static User createUser(String phoneNumber, String name){
         return new User(phoneNumber, name);
     }
 
+    /**
+     * Creates a new group based on parameters.
+     *
+     * @param groupName The name of the group.
+     * @param id The id of the group.
+     * @return A new group with specified properties and no members.
+     */
     public static Group createGroup(String groupName, String id){
         return new Group(groupName, id, new HashSet<>());
     }
